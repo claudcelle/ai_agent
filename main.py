6109976e569f8a9f0de61173ade6fc17230a998b
claudcelle <claudio.cellerini@gmail.com>
2025-10-23 22:20:54 +0200

    arg prompt

--- main.py
+++ main.py
@@ -6,14 +6,21 @@ import sys
 
 def main():
     
-    
+    try:
+        prompt = sys.argv[1]
+    except Exception as e: 
+        print(e)
+        sys.exit(1) 
+   
 
+    print(f'Input prompt: {prompt}')    
+    
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     # print(api_key[-5:])
     response = client.models.generate_content(model="gemini-2.0-flash-001",
-                                   contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+                                   contents = prompt)
     print(response.text)
     
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
